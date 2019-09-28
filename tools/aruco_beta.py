a2f4e7cd180ed9747f78167190e49c44671cf12a
Vladimir <48794941+1a11@users.noreply.github.com>
2019-09-28 11:47:44 +0300

    Aruco update

--- tools/aruco_beta.py
+++ tools/aruco_beta.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from cv2 import aruco
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+
 from ar_markers import detect_markers
 
 control = []
@@ -11,9 +11,9 @@ for i in range(100):
     control.append(i)
 frame = cv2.imread("markers_test.png")
 
-im = cv2.imread("markers_test.png")
+im = cv2.imread("markers_test - blank.png")
 i=0
-markers = detect_markers(frame)
+markers, c_markers = detect_markers(frame), detect_markers(im)
 marks = []
 for marker in markers:
     marker.highlite_marker(frame)
@@ -21,50 +21,65 @@ for marker in markers:
         #print(marker.id)
         marks.append(marker.id)
     i+=1
-plt.figure()
-plt.imshow(frame)
-plt.show()
-#print(markers)
 
 missing = []
 
+xy = []
+i2=0
 for i in control:
     if i in marks:
         pass
     else:
         missing.append(i)
 
-im[np.where((im == [0,0,0]).all(axis = 2))] = [255,255,255]
-im[np.where((im == [18,18,18]).all(axis = 2))] = [255,255,255]
-##im = cv2.imwrite("markers_test_res.png", crop_img)
-plt.figure()
-plt.imshow(im)
+for i in c_markers:
+    if i2%2 == 0:
+        print(int(i.id) in missing, i.id)
+        if int(i.id) in missing:
+            xy.append(i.center)
+    i2+=1
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create data
+x = []
+y = []
+
+
+for i in xy:
+    x.append(i[0])
+    y.append(i[1])
+
+color1 = (0,0,0)
+color2 = (255,0,0)
+area = np.pi*3
+
+x2 = [897 ,
+702 ,
+2064 ,
+2161 ,
+2064 ,
+2161 ,
+1869 ,
+1577 ,]
+y2 = [2807 ,
+2941 ,
+2003 ,
+1734 ,
+1601 ,
+1600 ,
+1466 ,
+1333 ,]
+
+# Plot
+plt.scatter(x, y, c='blue')
+plt.scatter(x2, y2, c='red')
+plt.title('Objects heatmap')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
 
-im = cv2.imwrite("markers_test_res.png", im)
-im = cv2.imread("markers_test_res.png")
-
-imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-
-nim = cv2.resize(imgray,(360,480))
-
-cv2.imshow('ImageWindow', nim)
-
-ret, thresh = cv2.threshold(imgray, 20, 255,cv2.THRESH_BINARY)
 
 
-nim = cv2.resize(thresh,(360,480))
-
-cv2.imshow('ImageWindow', nim)
- 
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
-
-cnt = contours[4]
-for contour in contours:
-        cv2.drawContours(im, cnt, -1, (0, 255, 0), 3)
-
-plt.figure()
-plt.imshow(im)
-plt.show()
