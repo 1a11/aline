d0210b93191d984ee77c5095562ef5ef3ff2afab
Yrello <52930426+Yrello@users.noreply.github.com>
2019-09-30 12:08:12 +0400

    Pic crop

---
+++ pic_cpor.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+im = Image.open("image.png")
+pixels = im.load()  # список с пикселями
+x, y = im.size  # ширина (x) и высота (y) изображения
+
+for i in range(64):
+    for j in range(48):
+        if i!=64 and j!=48:
+            im.crop(box=(x/64*i, y/48*j, x/64*(i+1)-1, y/48*(j+1)-1)).\
+            save('image{}_{}.png'.format(str(i+1), str(j+1)))
